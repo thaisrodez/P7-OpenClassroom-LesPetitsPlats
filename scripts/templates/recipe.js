4030b19a0ede129d08bd4761f397f4b80856c193
thais de rodez <thaisderodez@gmail.com>
2022-10-06 18:34:46 +0200

    Feat: initial commit

---
+++ scripts/templates/recipe.js
@@ -0,0 +1,9 @@
+class Recipe {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.ingredients = data.ingredients;
+    this.time = data.time;
+    this.description = data.description;
+  }
+}
